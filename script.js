0ae6088e1141da95032e4c85c039fc770a34dce7
Eric Johnson <johnsoneric586@gmail.com>
2024-05-24 06:58:50 -0400

    Created event listener to close modal window.

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
 'use strict';
+
+// ---------- Variables ----------
+
+// ----- DOM Elements -----
+const modal = document.getElementById('modal');
+const modalOverlay = document.getElementById('overlay');
+const btnCloseModal = document.getElementById('close-modal');
+
+// ---------- Functions ----------
+
+function closeModal() {
+  modal.classList.add('hidden');
+  modalOverlay.classList.add('hidden');
+}
+
+// ---------- Event Listeners ----------
+
+btnCloseModal.addEventListener('click', closeModal);
+modalOverlay.addEventListener('click', closeModal);
+document.addEventListener('keydown', function (event) {
+  if (!modal.classList.contains('hidden')) {
+    if (event.key === 'Escape') {
+      closeModal();
+    }
+  }
+});
